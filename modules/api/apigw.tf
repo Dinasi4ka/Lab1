2340aa715918247b14d595c6aad7599181c58d22
Dinasi4ka <viychukdiana@gmail.com>
2024-04-17 20:53:03 +0300

    some changes

--- modules/api/apigw.tf
+++ modules/api/apigw.tf
@@ -493,7 +493,7 @@ resource "aws_api_gateway_method_response" "put" {
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true,
     "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = false
+    "method.response.header.Access-Control-Allow-Origin" = true
   }
 }
 
